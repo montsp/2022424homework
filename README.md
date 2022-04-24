e74b6c31c07582c018c16ed69fc32d3931938fb0
montsp <konbuda4@icloud.com>
2022-04-24 11:12:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-eutukf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-eutukf)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>2022/4/24宿題</h1>
+<div>
+https://scratch.mit.edu/projects/679828174
+<iframe src="https://scratch.mit.edu/projects/679828174/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>
+https://scratch.mit.edu/projects/679828174
+<iframe src="https://scratch.mit.edu/projects/679829307/embed" allowtransparency="true" width="485" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>
+</div>`;
